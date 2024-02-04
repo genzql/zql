6b0e3be29f4275795a3d49b7fa3764435a497991
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-02-04 15:46:37 -0800

    feat: splash page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # zql
 SQL for Gen Z.
+
+## Getting started
+1) Ensure poetry installed.
+2) `poetry run uvicorn main:app --reload`
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request, "transpilation_result": ""})
+
+
+@app.post("/transpile")
+async def transpile_string(request: Request, inputString: str = Form(...)):
+    # transpilation logic here
+    transpilation_result = inputString.upper()
+
+    return templates.TemplateResponse("main.html",
+                                      {"request": request, "transpilation_result": transpilation_result})

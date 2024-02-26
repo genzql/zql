55be12075ddf857934f3c85761021a39fa1a39f9
Tamjid Rahman <tamjidarrahman@gmail.com>
2024-02-25 20:55:34 -0500

    feat: add shadcn

--- zql_ui/vite.config.ts
+++ zql_ui/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -9,4 +11,10 @@ export default defineConfig({
   },
 
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
